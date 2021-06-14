3ac516945f427f2016076ca9ea960e9541ea6e59
roscaandreea <andreea.rosca@info.uaic.ro>
2021-06-14 17:00:31 +0300

    Added final code for app

--- markdown-previewer/markdown_previewer/src/App.js
+++ markdown-previewer/markdown_previewer/src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import marked from 'marked';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState =`
+  This is a paragraph
+  
+  **This is bolded text**
+  
+  > Block Quotes!
+  
+  # Heading
+  ## Heading 2
+  
+  - list item 1
+  - list item 2
+  - list item 3
+  
+  [Visit this webiste](https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer)
+  
+  This is an inline \`<div></div>\`
+  
+  This is a block of code
+  \`\`\`
+    let x=10;
+    let y=20;
+    let t=x+y;
+  \`\`\`
+  
+  ![React](https://logos-download.com/wp-content/uploads/2016/09/React_logo_wordmark.png)  
+`
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: initialState
+    };
+    this.previewMarkdown = this.previewMarkdown.bind(this);
+  }
+  
+  updateMarkdown = (event) => {
+    this.setState({
+      markdown: event.target.value
+    });
+  }
+
+  previewMarkdown() {
+    let markup = marked(this.state.markdown,{breaks: true});
+    return { __html: markup };
+  }
+
+ 
+  render(){
+    return(
+      <div className="bid">
+        <h2 className="text-center m-4"><i className="fa fa-code"></i>Convert your Markdown</h2>
+        <div className="row">
+          <div className="col-6">
+            <h6 className="text-center">Enter your markdown here:</h6>
+            <div className="toolbar">
+              <h5 className="textEditor ml-2"><i className="fa fa-edit"></i>editor</h5>
+            <textarea className="form-control p-2" id="editor"  value={this.state.markdown} onChange={this.updateMarkdown}></textarea>
+            </div>
+          </div>
+          <div className="col-6">
+            <h6 className="text-center">See the result:</h6>
+             <div className="toolbar">
+               <h5 className="textEditor ml-2"><i className="fa fa-eye"></i>previewer</h5>
+            <div dangerouslySetInnerHTML={this.previewMarkdown()} className="preview p-2" id="preview" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
