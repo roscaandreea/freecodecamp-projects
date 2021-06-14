f78838223acd87b2a7fe4ce0a876dc3dd47f778e
roscaandreea <andreea.rosca@info.uaic.ro>
2021-06-14 20:27:37 +0300

    Make to work font-awesome fot buttons

--- pomodoro-clock/pomodoro_clock/src/App.js
+++ pomodoro-clock/pomodoro_clock/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 const audio= document.getElementById("beep");
 
@@ -169,8 +168,8 @@ class App extends React.Component{
           <h1 id="timer-label">{currentTimer}</h1>
           <span  id="time-left">{this.convertToTime(clockCount)}</span>
           <div className="flex">
-              <button id="start_stop" onClick={this.handlePlayPause}><i className={`fas fa-${isPlaying ? `pause`: `play`}`}/></button>
-              <button id="reset" onClick={this.handleReset}><i className="fas fa-sync"/></button>
+              <button id="start_stop" onClick={this.handlePlayPause}><i className={`fa fa-${isPlaying ? `pause`: `play`}`}/></button>
+              <button id="reset" onClick={this.handleReset}><i className="fa fa-refresh"/></button>
           </div>
         </div>
       </div>
@@ -184,9 +183,9 @@ const SetTimer = (props) => {
     <div className="timer-container">
       <h2 id={`${id}-label`}>{props.title} Length</h2>
       <div className="flex actions-wrapper">
-        <button id={`${id}-decrement`} onClick={props.handleDecrease}><i className="fas fa-minus"/></button>
+        <button id={`${id}-decrement`} onClick={props.handleDecrease}><i className="fa fa-minus"/></button>
         <span id={`${id}-length`}>{props.count}</span>
-        <button id={`${id}-increment`} onClick={props.handleIncrease}><i className="fas fa-plus"/></button>
+        <button id={`${id}-increment`} onClick={props.handleIncrease}><i className="fa fa-plus"/></button>
       </div>
     </div>
   );
